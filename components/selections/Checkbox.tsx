8e96869011c68c27b0216f80ab6075d0cd83610b
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-14 13:21:27 +0900

    FIX: CheckBox 컴포넌트 수정

--- components/selections/Checkbox.tsx
+++ components/selections/Checkbox.tsx
@@ -1,24 +1,51 @@
 const style = {
-  basic:
-    "w-[24px] h-[24px] bg-[url('/icon/selections/checkBox-b-x.svg')] checked:bg-[url('/icon/selections/checkBox-b-o.svg')]",
-  round:
-    "w-[24px] h-[24px] bg-[url('/icon/selections/checkBox-r-x.svg')] checked:bg-[url('/icon/selections/checkBox-r-o.svg')]",
-  small:
-    "w-[16px] h-[16px] bg-[url('/icon/selections/checkBox-s-x.svg')] checked:bg-[url('/icon/selections/checkBox-s-o.svg')]",
+  black: {
+    basic:
+      "bg-[url('/icon/selections/checkBox-black-basic-x.svg')] checked:bg-[url('/icon/selections/checkBox-black-basic-o.svg')]",
+    round:
+      "bg-[url('/icon/selections/checkBox-black-round-x.svg')] checked:bg-[url('/icon/selections/checkBox-black-round-o.svg')]",
+    square:
+      "bg-[url('/icon/selections/checkBox-black-square-x.svg')] checked:bg-[url('/icon/selections/checkBox-black-square-o.svg')]",
+  },
+  red: {
+    basic:
+      "bg-[url('/icon/selections/checkBox-red-basic-x.svg')] checked:bg-[url('/icon/selections/checkBox-red-basic-o.svg')]",
+    round:
+      "bg-[url('/icon/selections/checkBox-red-round-x.svg')] checked:bg-[url('/icon/selections/checkBox-red-round-o.svg')]",
+    square:
+      "bg-[url('/icon/selections/checkBox-red-square-x.svg')] checked:bg-[url('/icon/selections/checkBox-red-square-o.svg')]",
+  },
 };
-interface CheckboxProps {
-  type?: "basic" | "round" | "small";
+
+const sizeStyle = {
+  S: "w-[16px] h-[16px]",
+  L: "w-[24px] h-[24px]",
+};
+
+interface CheckboxProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "size"
+  > {
+  type?: "basic" | "round" | "square";
+  color?: "black" | "red";
+  size?: "S" | "L";
 }
 
 export default function Checkbox({
   type = "basic",
+  color = "black",
+  size = "L",
   ...props
-}: CheckboxProps & JSX.IntrinsicElements["input"]) {
+}: CheckboxProps) {
   return (
     <input
       {...props}
       type="checkbox"
-      className={`appearance-none ${style[type]}`}
+      className={`appearance-none bg-cover ${sizeStyle[size]} ${style[color][type]}`}
     />
   );
 }
