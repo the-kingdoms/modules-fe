5be601a57d70e1cc9397412e11756003b56cc54f
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-20 15:23:56 +0900

    FIX: layout 컴포넌트 classname 추가

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,14 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { HTMLAttributes } from "react";
+
+export default function Layout({
+  className,
+  children,
+}: {
+  className?: HTMLAttributes<HTMLDivElement>[`className`];
+  children: React.ReactNode;
+}) {
   return (
-    <div className="bg-white" style={{ height: "100dvh" }}>
+    <div className={className} style={{ height: "100dvh" }}>
       <div className="w-screen h-full max-w-[360px] m-auto">{children}</div>
     </div>
   );
