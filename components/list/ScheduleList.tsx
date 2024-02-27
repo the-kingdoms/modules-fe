efc3f320cb7b940935da1eee063cdabc6cdfb4de
DeveloperRyou <DeveloperRyou@gmail.com>
2024-02-27 14:15:52 +0900

    FIX: ScheduleList animation 추가

--- components/list/ScheduleList.tsx
+++ components/list/ScheduleList.tsx
@@ -1,6 +1,7 @@
 import FlexBox from "@modules/layout/FlexBox";
 import Icon from "@modules/layout/Icon";
 import { useState } from "react";
+import styles from "./ScheduleList.module.css";
 
 interface ScheduleListProps {
   name: string;
@@ -21,37 +22,35 @@ export default function ScheduleList({
   time,
   onDelete,
 }: ScheduleListProps) {
-  const [selectList, setSelectList] = useState(false);
+  const [selectList, setSelectList] = useState<boolean | null>(null);
 
   const clickList = () => {
-    setSelectList(!selectList);
+    if (selectList === null) setSelectList(true);
+    else setSelectList(!selectList);
   };
 
   return (
-    <FlexBox
-      direction="row"
-      className={`w-full h-14 ${selectList ? "" : "pl-12"}`}
-    >
+    <FlexBox direction="row" className="w-full h-14 relative">
       <button
         type="button"
-        className={`w-[280px] h-full ${style[position]}
-            flex flex-col items-start gap-1 px-3 py-2.5
-            ${selectList ? "rounded-l-xl" : "rounded-xl"}`}
+        className={`absolute right-0 w-[280px] h-full rounded-xl ${style[position]} ${selectList ? styles.appear : styles.disappear}`}
         onClick={clickList}
       >
-        <div className="B5-regular text-white">{time}</div>
-        <div className="B4-regular text-white">{name}</div>
+        <FlexBox direction="col" className="items-start gap-1 px-3 py-2.5">
+          <div className="B5-regular text-white">{time}</div>
+          <div className="B4-regular text-white">{name}</div>
+        </FlexBox>
       </button>
-      {selectList && (
+      <div className="ml-auto h-full bg-Gray6 rounded-r-xl w-12">
         <button
           type="button"
-          className="w-12 h-full bg-Gray6 rounded-r-xl flex items-center justify-center"
+          className="h-full w-full flex items-center justify-center"
           onClick={onDelete}
         >
           <span className="hidden">delete</span>
           <Icon src="/icon/control/delete.svg" sz={18} />
         </button>
-      )}
+      </div>
     </FlexBox>
   );
 }
