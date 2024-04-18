253e99a179010160db8bb95923bf860e7328c1eb
DeveloperRyou <DeveloperRyou@gmail.com>
2024-04-18 18:19:52 +0900

    FIX: Apple login 추가

--- components/button/LoginButton.tsx
+++ components/button/LoginButton.tsx
@@ -2,7 +2,7 @@ import FlexBox from "../../layout/FlexBox";
 import Icon from "../../layout/Icon";
 
 interface LoginButtonProps {
-  type: "google" | "naver" | "kakao";
+  type: "google" | "naver" | "kakao" | "apple";
   onClick: () => void;
 }
 
@@ -10,24 +10,28 @@ const style = {
   google: "bg-White border border-Gray3",
   naver: "bg-[#03C75A]",
   kakao: "bg-[#FEE500]",
+  apple: "bg-[#FFFFFF]",
 };
 
 const textStyle = {
   google: "text-Gray4",
   naver: "text-White",
   kakao: "text-Black",
+  apple: "text-Black",
 };
 
 const text = {
   google: "구글로 계속하기",
   naver: "네이버로 계속하기",
   kakao: "카카오 로그인",
+  apple: "Apple로 계속하기",
 };
 
 const src = {
   google: "/icon/login/google.png",
   naver: "/icon/login/naver.png",
   kakao: "/icon/login/kakao.png",
+  apple: "/icon/login/apple.png",
 };
 
 export default function LoginButton({ type, onClick }: LoginButtonProps) {
