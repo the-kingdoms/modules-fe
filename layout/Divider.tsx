2c68eb675bcb5cd15ca507d247b6347e15d4fce6
PangKiYeon <pdrkeamy@gmail.com>
2024-06-03 22:14:48 +0900

    REMOVE : conflict 해결 위한 제거

--- layout/Divider.tsx
+++
@@ -1,8 +0,0 @@
-export default function Divider({ height = 1 }: { height?: number }) {
-  return (
-    <div
-      className="border-t border-Gray2 bg-Gray1 w-full"
-      style={{ height: `${height}px` }}
-    />
-  );
-}
