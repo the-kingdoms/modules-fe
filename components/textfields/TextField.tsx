26b25615175fa445cd7096f8ad7830f2f2db317f
DeveloperRyou <DeveloperRyou@gmail.com>
2024-07-01 17:36:06 +0900

    FIX: textfield 컴포넌트 수정

--- components/textfields/TextField.tsx
+++ components/textfields/TextField.tsx
@@ -1,106 +1,117 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import FlexBox from "../../layout/FlexBox";
-import Icon from "../../layout/Icon";
 
 interface TextFieldProps {
-  value?: string;
-  setValue?: React.Dispatch<React.SetStateAction<string>>;
-  label?: string;
-  assistText?: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
   className?: string;
-  isError?: boolean;
-  button?: React.ReactNode;
+  setIsFocused?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 /**
- * TextField 컴포넌트는 얼루가의 텍스트 필드를 구현한 컴포넌트입니다.
+ * TimeField 컴포넌트는 얼루가의 시간 필드를 구현한 컴포넌트입니다.
  * useState를 통해 value와 setValue를 받아서 사용합니다.
- * @param value - 텍스트 필드의 값 (state)
- * @param setValue - 텍스트 필드의 값 변경 함수 (setState)
- * @param label - 텍스트 필드의 레이블
- * @param assistText - 텍스트 필드의 보조 텍스트
- * @param className - 텍스트 필드의 클래스, 없을 경우 B3-regular로 설정됩니다
- * @param isError - 텍스트 필드의 에러 여부, true일 경우 빨간색으로 표시됩니다.
- * @param button - 텍스트 필드의 오른쪽에 위치할 버튼, ReactNode로 받습니다.
- * @param props - html input의 모든 속성을 받습니다.
+ * @param value - 시간 필드의 값 (state), 4자리의 숫자로 구성된 문자열을 넣어야합니다.
+ * @param setValue - 시간 필드의 값 변경 함수 (setState)
+ * @param className - 시간 필드의 클래스
  * @returns
  */
-export default function TextField({
+export default function TimeField({
   value,
   setValue,
-  label,
-  assistText,
   className,
-  isError,
-  button,
+  setIsFocused,
   ...props
 }: TextFieldProps & JSX.IntrinsicElements["input"]) {
   const [focus, setFocus] = useState<boolean>(false);
+  const ref0 = useRef<HTMLInputElement>(null);
+  const ref1 = useRef<HTMLInputElement>(null);
+  const ref2 = useRef<HTMLInputElement>(null);
+  const ref3 = useRef<HTMLInputElement>(null);
+  const inputRefList = [ref0, ref1, ref2, ref3];
 
-  const onClickCancel = () => {
-    setValue && setValue("");
+  const handleFocus = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    inputRefList[0].current?.focus();
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue && setValue(e.target.value);
-    props.onChange && props.onChange(e);
+  const handleChange = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    const { key } = e;
+    if (key === "Backspace") {
+      setValue(`${value.slice(0, index)}0${value.slice(index + 1)}`);
+      if (index > 0) {
+        inputRefList[index].current?.blur();
+        inputRefList[index - 1].current?.focus();
+      }
+      return;
+    }
+    if (Number.isNaN(Number(key))) {
+      setValue(value);
+      return;
+    }
+    setValue(value.slice(0, index) + key + value.slice(index + 1));
+    if (index < 3) {
+      inputRefList[index].current?.blur();
+      inputRefList[index + 1].current?.focus();
+    }
   };
 
-  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    setFocus(true);
-    props.onFocus && props.onFocus(e);
-  };
-
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    setFocus(false);
-    props.onBlur && props.onBlur(e);
-  };
+  useEffect(() => {
+    focus && setIsFocused !== undefined && setIsFocused(true);
+    !focus && setIsFocused !== undefined && setIsFocused(false);
+  }, [focus]);
 
   return (
-    <FlexBox direction="col" className="w-full gap-1">
-      {label && (
-        <label
-          className="B4-regular leading-[16px] w-full text-Gray6"
-          htmlFor={props.id}
-        >
-          {label}
-        </label>
-      )}
-      <FlexBox
-        className={`w-full justify-between h-[56px] border-b-2 ${
-          isError ? "border-Red" : focus ? "border-Black" : "border-Gray3"
-        }`}
-      >
-        <input
-          {...props}
-          className={`w-full h-full bg-transparent outline-none ${
-            isError ? "text-Red" : focus ? "text-black" : "text-Gray5"
-          } ${className || "B3-regular"} placeholder:text-Gray3`}
-          value={value}
-          onChange={handleChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-        {button}
-        {!button && focus && (
-          <Icon
-            src={`/icon/textfields/close_${isError ? "red" : "black"}.png`}
-            alt="x"
-            sz={20}
-            className="cursor-pointer"
-            onClick={onClickCancel}
+    <FlexBox
+      className={`justify-center w-[148px] h-[40px] border border-1 rounded ${
+        props.disabled ? "bg-Gray1" : focus ? "bg-black" : "bg-Gray6"
+      } ${props.disabled ? "text-Gray3" : focus ? "text-white" : "text-Gray3"}`}
+    >
+      <button type="button" className="w-full h-full cursor-text" onClick={handleFocus}>
+        <FlexBox className="justify-center">
+          <input
+            ref={ref0}
+            value={value[0]}
+            className="outline-none w-[12px] text-center selection:underline bg-transparent"
+            onKeyDown={(e) => handleChange(e, 0)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            pattern="[0-9]*"
+            inputMode="numeric"
           />
-        )}
-      </FlexBox>
-      {assistText && (
-        <p
-          className={`w-full C2 leading-[16px] ${
-            isError ? "text-Red" : focus ? "text-Gray7" : "text-Gray5"
-          }`}
-        >
-          {assistText}
-        </p>
-      )}
+          <input
+            ref={ref1}
+            value={value[1]}
+            className="outline-none w-[12px] text-center selection:underline bg-transparent"
+            onKeyDown={(e) => handleChange(e, 1)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            pattern="[0-9]*"
+            inputMode="numeric"
+          />
+          <span className="mx-1">:</span>
+          <input
+            ref={ref2}
+            value={value[2]}
+            className="outline-none w-[12px] text-center selection:underline bg-transparent"
+            onKeyDown={(e) => handleChange(e, 2)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            pattern="[0-9]*"
+            inputMode="numeric"
+          />
+          <input
+            ref={ref3}
+            value={value[3]}
+            className="outline-none w-[12px] text-center selection:underline bg-transparent"
+            onKeyDown={(e) => handleChange(e, 3)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            pattern="[0-9]*"
+            inputMode="numeric"
+          />
+        </FlexBox>
+      </button>
     </FlexBox>
   );
 }
