dab69584db118bc693c57dd56f0a4819bb791011
PangKiYeon <pdrkeamy@gmail.com>
2024-07-01 19:32:17 +0900

    FIX : pr 검증 오류 해결

--- src/icon/Icon.tsx
+++ src/icon/Icon.tsx
@@ -1,64 +1,64 @@
-import * as Commerce from "./svg/Commerce";
-import * as Control from "./svg/Control";
-import * as Data from "./svg/Data";
-import * as File from "./svg/File";
-import * as Formatting from "./svg/Formatting";
-import * as Modifier from "./svg/Modifier";
-import * as Navigation from "./svg/Navigation";
-import * as Social from "./svg/Social";
-import * as Status from "./svg/Status";
-import * as Operation from "./svg/Operation";
-import * as Technology from "./svg/Technology";
-import * as Time from "./svg/Time";
-import * as Toggle from "./svg/Toggle";
-import * as User from "./svg/User";
+import * as Commerce from './svg/Commerce/index';
+import * as Control from './svg/Control/index';
+import * as Data from './svg/Data/index';
+import * as File from './svg/File/index';
+import * as Formatting from './svg/Formatting/index';
+import * as Modifier from './svg/Modifier/index';
+import * as Navigation from './svg/Navigation/index';
+import * as Social from './svg/Social/index';
+import * as Status from './svg/Status/index';
+import * as Operation from './svg/Operation/index';
+import * as Technology from './svg/Technology/index';
+import * as Time from './svg/Time/index';
+import * as Toggle from './svg/Toggle/index';
+import * as User from './svg/User/index';
 
 export const IconCategory = {
-  Commerce: { ...Commerce },
-  Control: { ...Control },
-  Data: { ...Data },
-  File: { ...File },
-  Formatting: { ...Formatting },
-  Modifier: { ...Modifier },
-  Navigation: { ...Navigation },
-  Social: { ...Social },
-  Status: { ...Status },
-  Operation: { ...Operation },
-  Technology: { ...Technology },
-  Time: { ...Time },
-  Toggle: { ...Toggle },
-  User: { ...User },
+    Commerce: { ...Commerce },
+    Control: { ...Control },
+    Data: { ...Data },
+    File: { ...File },
+    Formatting: { ...Formatting },
+    Modifier: { ...Modifier },
+    Navigation: { ...Navigation },
+    Social: { ...Social },
+    Status: { ...Status },
+    Operation: { ...Operation },
+    Technology: { ...Technology },
+    Time: { ...Time },
+    Toggle: { ...Toggle },
+    User: { ...User },
 };
 
 const icons = {
-  ...Commerce,
-  ...Control,
-  ...Data,
-  ...File,
-  ...Formatting,
-  ...Modifier,
-  ...Navigation,
-  ...Social,
-  ...Status,
-  ...Operation,
-  ...Technology,
-  ...Time,
-  ...Toggle,
-  ...User,
+    ...Commerce,
+    ...Control,
+    ...Data,
+    ...File,
+    ...Formatting,
+    ...Modifier,
+    ...Navigation,
+    ...Social,
+    ...Status,
+    ...Operation,
+    ...Technology,
+    ...Time,
+    ...Toggle,
+    ...User,
 };
 
 export type iconKey = keyof typeof icons;
 
 type IconProps = {
-  /**
-   * 아이콘 타입
-   */
-  icon: iconKey;
-  /**
-   * 아이콘 크기
-   */
-  size?: number;
-  className?: string;
+    /**
+     * 아이콘 타입
+     */
+    icon: iconKey;
+    /**
+     * 아이콘 크기
+     */
+    size?: number;
+    className?: string;
 };
 
 /**
@@ -66,11 +66,9 @@ type IconProps = {
  */
 
 const Icon = ({ icon, size = 24, className }: IconProps) => {
-  const SVGIcon = icons[icon];
+    const SVGIcon = icons[icon];
 
-  return (
-    <SVGIcon className={className} width={size ?? 24} height={size ?? 24} />
-  );
+    return <SVGIcon className={className} width={size ?? 24} height={size ?? 24} />;
 };
 
 export default Icon;
